8e0e73d8450a55fbaffaf3621b4dfc75e32ff910
Anh Duong <anhhduong1609@gmail.com>
2022-06-29 23:53:06 -0400

    add README.md and about page

--- pixabay-image-search/src/components/About.js
+++ pixabay-image-search/src/components/About.js
@@ -2,9 +2,21 @@ import React from "react";
 
 function About() {
   return (
-    <div style={{ padding: "1rem" }}>
-      Welcome to ImageSearch! Navigate to Home Tab and use the search bar to
-      search any images you like. Have fun!
+    <div style={{ display: "flex" }}>
+      <div
+        style={{
+          padding: "1rem",
+          textAlign: "center",
+          backgroundImage: `url(robotbackground.jpg)`,
+          backgroundSize: "cover",
+          height: 500,
+          width: 500,
+        }}
+      />
+      <div>
+        Welcome to ImageSearch! Navigate to Home Page to search for images. Have
+        fun!
+      </div>
     </div>
   );
 }
